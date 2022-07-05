8856a074e985da8ff199b6065a958e206df4fdab
Mohit Aswani <72434608+MohitAswani@users.noreply.github.com>
2022-07-05 14:21:38 +0530

    Fixing error.

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import logo from "./logo.png";
 import "./App.css";
 import Navigation from "./Navbar";
 import { useState } from "react";
